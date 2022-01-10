0587fe262c81ca632eafd1563a52fb758940a6ce
NanoLoriginal <kyliann.faggian@gmail.com>
2022-01-10 15:55:05 +0100

    Initial commit

---
+++ src/Tableau1.js
@@ -0,0 +1,103 @@
+class Tableau1 extends Phaser.Scene{
+
+    preload() {
+        this.load.image('carre',"/assets/carre.png");
+        this.load.image('cercle',"/assets/cercle.png");
+    }
+
+    create(){
+        this.largeur = 500
+        this.longueur = 1000
+
+        this.haut = this.physics.add.image(0,0,"carre").setOrigin(0,0);
+        this.haut.setDisplaySize(1000,20);
+        this.haut.body.setAllowGravity(0);
+        this.haut.setImmovable(true);
+
+
+        this.bas = this.physics.add.image(0,500,"carre").setOrigin(0,1);
+        this.bas.setDisplaySize(1000,20)
+        this.bas.body.setAllowGravity(0);
+        this.bas.setImmovable(true);
+
+        this.balle = this.physics.add.image(this.longueur/2,this.largeur/2,"cercle");
+        this.balle.setDisplaySize(20,20);
+        this.balle.body.setBounce(1.1,1.1)
+        this.balle.setVelocityX(Phaser.Math.Between(-400,400));
+        this.balle.setVelocityY(Phaser.Math.Between(-400,400));
+        this.balle.body.setMaxVelocity(1000,1000);
+
+        this.p1 = this.physics.add.image(0,20,"carre").setOrigin(0,0);
+        this.p1.setDisplaySize(20,100)
+        this.p1.body.setAllowGravity(0);
+        this.p1.setImmovable(true);
+
+
+        this.p2 = this.physics.add.image(1000,20,"carre").setOrigin(1,0);
+        this.p2.setDisplaySize(20,100)
+        this.p2.body.setAllowGravity(0);
+        this.p2.setImmovable(true);
+
+
+
+        this.physics.add.collider(this.balle,this.bas);
+        this.physics.add.collider(this.balle,this.haut);
+        this.physics.add.collider(this.p1,this.balle);
+        this.physics.add.collider(this.p2,this.balle);
+
+        this.initKeyboard();
+
+    }
+
+    initKeyboard(){
+        let me=this;
+        this.input.keyboard.on('keydown', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                    me.p1.setVelocityY(150);
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.Z:
+                    me.p1.setVelocityY(-150);
+                    break;
+            }
+        });
+
+        this.input.keyboard.on('keyup', function(kevent)
+        {
+            switch (kevent.keyCode)
+            {
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                    me.p1.setVelocityY(0);
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.Z:
+                    me.p1.setVelocityY(0);
+                    break;
+            }
+        });
+
+
+
+    }
+
+    update() {
+        if(this.balle.x>this.longueur){
+            this.balle.x = 0
+        }
+        if(this.balle.x<0){
+            this.balle.x = this.longueur
+        }
+
+        if(this.balle.y<0){
+            this.balle.y = 0
+        }
+        if(this.balle.y>this.largeur){
+            this.balle.y = this.largeur
+        }
+
+
+    }
+
+
+}
---
+++ src/main1.js
@@ -0,0 +1,17 @@
+/*configuration*/
+
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 500,
+    backgroundColor: '#ffffff',
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: new Tableau1()
+};
+let game = new Phaser.Game(gameConfig);
